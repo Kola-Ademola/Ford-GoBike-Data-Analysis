eb77f96a30761b0ce2435551eac6b1322d487834
Edwin <76483317+Eddy265@users.noreply.github.com>
2023-03-10 17:19:51 +0100

    Update kola.py

--- kola.py
+++ kola.py
@@ -1,11 +1,1 @@
 import pandas as pd
-<<<<<<< HEAD
-import matplotlib as plt
-
-
-#commit me now
-=======
-import numpy as np
-# comment out
-# new comment
->>>>>>> 51bb07ee6642c9a4999f5ac07d2e820552671117
